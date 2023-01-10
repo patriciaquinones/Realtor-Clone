9f1f28b90eda7103955bc34c8f354e5599ce3be8
Patricia Quiñones <patriciaqbautista@gmail.com>
2023-01-09 23:48:04 -0400

    Create the landlord contact component

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -7,12 +7,16 @@ import {Swiper, SwiperSlide} from 'swiper/react';
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from 'swiper';
 import 'swiper/swiper-bundle.css';
 import {FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair} from 'react-icons/fa';
+import { getAuth } from 'firebase/auth';
+import Contact from '../components/Contact';
 
 export default function Listing() {
+    const auth = getAuth();
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
     const [shareLinkCopied, setShareLinkCopied] = useState(false);
+    const [contactLandlord, setContactLandlord] = useState(false);
     SwiperCore.use([Autoplay, Navigation, Pagination, EffectFade]);
     useEffect (() => {
         async function fetchListing() {
@@ -54,9 +58,10 @@ export default function Listing() {
             )}
 
             <div className='m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5'>
-                <div className='w-full h-[200px] lg-[400px]'>
+                <div className='w-full '>
                     <p className='text-2xl font-bold mb-3 text-blue-900'>
-                        {listing.name} - $ {listing.offer ? listing.discountedPrice .toString()
+                        {listing.name} - $ {listing.offer ? listing.discountedPrice
+                            .toString()
                             .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
                             : listing.regularPrice
                             .toString()
@@ -67,14 +72,16 @@ export default function Listing() {
                     <div className='flex justify-start items-center space-x-4 w-[75%]'>
                         <p className='bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md'>{listing.type === "rent" ? "Rent" : "Sale"}</p>
                         {listing.offer && (
-                            <p className='bg-green-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md'>$ {+listing.regularPrice - +listing.discountedPrice} discount</p>
+                            <p className='bg-green-800 w-full max-w-[200px] rounded-md pt-1 pb-1 text-white text-center font-semibold shadow-md'>${(+listing.regularPrice - +listing.discountedPrice)
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")} discount</p>
                         )}
                     </div>
                     <p className='mt-3 mb-3'>
                         <span className='font-semibold'>Description - </span>
                         {listing.description}
                     </p>
-                    <ul className='flex items-center sm:space-x-2 space-x-10 text-sm font-semibold'>
+                    <ul className='flex items-center sm:space-x-2 space-x-10 text-sm font-semibold mb-6'>
                         <li className='flex items-center whitespace-nowrap '>
                             <FaBed className='text-lg mr-1'/>
                             {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
@@ -95,6 +102,16 @@ export default function Listing() {
                             {listing.furnished ? "Furnished" : "Not furnished"}
                         </li>
                     </ul>
+                    {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+                        <div className='mt-6'>
+                            <button 
+                                onClick={()=>setContactLandlord(true)} 
+                                className='px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out'>Contact Landlord</button>
+                        </div>
+                    )}
+                    {contactLandlord && (
+                        <Contact userRef={listing.userRef} listing={listing}/>
+                    )}
                 </div>
                 <div className='bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden'>
 
